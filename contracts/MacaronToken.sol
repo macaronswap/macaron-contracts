ec7d5458ed1e7f710791ccdeac01562139cdb3cb
Macaron Chef <macaronswap@gmail.com>
2021-09-22 22:58:29 +0200

    Update MacaronToken.sol

--- contracts/MacaronToken.sol
+++ contracts/MacaronToken.sol
@@ -1100,7 +1100,7 @@ contract BEP20 is Context, IBEP20, Ownable {
 }
 
 // MockMacaron with Governance.
-contract MacaronToken is BEP20('Mock MCRN', 'MCRN') {
+contract MacaronToken is BEP20('MCRN', 'MCRN') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
